480816d0c27326529077493681587843421221a6
Melisa06 <mel.avila0906@gmail>
2018-02-28 20:42:14 +0000

    add an other sh and documented code

--- curso.sh
+++ curso.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-
+##Primero limpiamos la pantalla 
 clear
+##inicio de la presentación de edi 
 echo "Hola, Dime cuál es tu nombre? "
-
+## asignamos i¿una variable de tipo read que lea lo que el 
+##teclado escribe y lo guardamos en una variable nombre 
 read nombre 
-
+####Comenzamos con la presentacíon de edison 
 echo "Hola $nombre bienvenido al laboratorio"
 echo " "
 echo "Mi nombre es Edison, pero a Melisa le gusta llamarme Edi"
@@ -13,26 +15,35 @@ sleep 3s #wait x seconds
 echo "Estoy listo para jugar contigo"
 echo " "
 sleep 3s
+
+##Mostramos lo que puede hacer
 echo "¿Qué quieres hacer?"
  echo "1) Conocer Temperatura y Humedad"
  echo "2) Cambiar colores" 
  echo "3) Otro "
+ ##realizamos un While con case 
 while : 
 do 
-read INPUT_STRING 
-case $INPUT_STRING in 
+##Lee el teclado 
+read INPUT_STRING
+##y entra al case 
+case $INPUT_STRING in
+      ##En caso de escribir el número uno haro lo siguiente 	
  1) echo "Haz elegido conocer la temperatura y humedad"
     python sensores.py    
    ;;
-
+##En caso de escribir el numero 2  hará lo siguiente
  2) echo "Haz elegido cambiar colores a la pantalla"
 	./colores.sh  
 	 ;;
  3) echo "otro"
   ;;
- *) echo "Esta no es una opcion del menu"
-  break
-  ;;
 
+  ##en caso de escribir algo de lo que no esta seleccionado mostrara otro 
+  ##Menu 
+ *) echo "Esta no es una opcion del menu"
+   ./menu.sh	
+  ;;
+##termina el case
 esac 
 done
---
+++ menu.sh
@@ -0,0 +1,34 @@
+
+#!/bin/sh 
+
+
+##Mostrar el menu 
+echo "*** MENU ***"
+echo "Que quieres hacer ahora?"
+
+echo "1) Conocer la temperatura y Humedad" 
+echo "2) Cambiar colores"
+echo "3) Otro"
+
+###Elegir las opciones 
+while : 
+do 
+	##LEER EL TECLADO 
+	read INPUT_STRING
+       case $INPUT_STRING in
+	  #S√≠ seleccionamos la opcion uno conoceremos la temperatura y humedad 
+	       1) echo "Veamos la tamperatura y la humedad"
+		       python sensores.py
+		       ;;
+ ##Si elegimos la opci√n 2 cambiaremos los colores de la LCD 
+	       2) echo "Cambiemos los coloresa la pantalla" 
+		       ./colores.sh
+		       ;;
+	       3) echo "Otro" 
+		       ;; 
+	       *)echo "No has seleccionado una opcion valida"
+		       
+		       break
+		       ;; 
+       esac 
+       done 
