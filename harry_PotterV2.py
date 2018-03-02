6934041f1dd7b6390d4e59f40668525e5189c6de
Melisa06 <mel.avila0906@gmail>
2018-03-02 20:08:42 +0000

    Cancion Harry Potter

---
+++ harry_PotterV2.py
@@ -0,0 +1,23 @@
+import time 
+from upm import pyupm_buzzer as upmBuzzer
+
+buzzer =upmBuzzer.Buzzer(5)
+buzzer.setVolume(0.2)
+
+buzzer.playSound(1620,400000)
+buzzer.playSound(1213,600000)
+buzzer.playSound(1020,200000)
+buzzer.playSound(1145,400000)
+buzzer.playSound(1213,800000)
+buzzer.playSound(810,400000)
+buzzer.playSound(909,1000000)
+buzzer.playSound(1081,1000000)
+buzzer.playSound(1213,600000)
+buzzer.playSound(1020,200000)
+buzzer.playSound(1287,800000)
+buzzer.playSound(1145,400000)
+buzzer.playSound(1620,160000)
+
+del buzzer
+
+
